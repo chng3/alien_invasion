f5332cca6a28e9cd37ed437e1e66459d08548753
dolphinchng <chngdolphin@gmail.com>
2018-12-09 21:22:29 +0800

    飞船移动功能完成

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,22 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+	"""响应按键"""
+	
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+		
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+		
+def check_keyup_events(event, ship):
+	"""响应松开"""
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+		
 def check_events(ship):
 	"""响应按键和鼠标事件"""
 	for event in pygame.event.get():
@@ -9,16 +25,10 @@ def check_events(ship):
 			sys.exit()
 			
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = True
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = True
+			check_keydown_events(event, ship)
 				
 		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_right = False
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = False
+			check_keyup_events(event, ship)
 				
 def update_screen(ai_settings, screen, ship):
 	"""更新屏幕上的图像，并切换到新屏幕"""
--- settings.py
+++ settings.py
@@ -10,3 +10,6 @@ class Settings():
 		self.screen_height = 700
 		self.bg_color = (230, 230, 230)
 		
+		# 飞船的设置
+		self.ship_speed_factor = 1.5
+		
